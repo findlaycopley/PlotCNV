c8934888271027dbe579fed030c1540fe0a74d0e
findlaycopley@hotmail.co.uk <findlaycopley@hotmail.co.uk>
2020-07-30 16:36:54 +0100

    Fixing the problem with lengths

--- R/getGenomeLengths.R
+++ R/getGenomeLengths.R
@@ -14,8 +14,15 @@
 getGenomeLengths <- function(ReturnClass, genome="hg19", FinalChrom="chrX") {
         ## This function will download the chromosome sizes from UCSC
         Chr_Sizes <- getChromInfoFromUCSC(genome)
+
         CutOff <- Chr_Sizes$chrom %>% grepl(paste(FinalChrom,"$", sep=""), .) %>% which()
         Chr_Sizes <- Chr_Sizes[1:CutOff,]
-        Chr_Sizes <- c(Chr_Sizes$length) %>% setNames(Chr_Sizes$chrom)
+        if( "size" %in% colnames(Chr_Sizes)) {
+                Chr_Sizes <- c(Chr_Sizes$size) %>% setNames(Chr_Sizes$chrom)
+        } else if ( "length" %in% colnames(Chr_Sizes)) {
+                Chr_Sizes <- c(Chr_Sizes$length) %>% setNames(Chr_Sizes$chrom)
+        } else {
+                Chr_Sizes <- c(Chr_Sizes[,2] %>% unlist()) %>% setNames(Chr_Sizes$chrom)
+        }
         Chr_Sizes
 }
